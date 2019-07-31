e406223ebd9f950bdfe2823f666e11b1ad103fee
widyagading <widya_gading_27rpl@student.smktelkom-mlg.sch.id>
2019-07-31 10:00:56 +0700

    Controller

---
+++ kentang.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class kentang extends Controller
+{
+    //
+    public function sandal() {
+      echo "sandal swallooowww";
+    }
+}
---
+++ project1.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class project1 extends Controller
+{
+    //ditambahi sedikit
+    public function index() {
+      echo "ini dari index punya usang controller";
+    }
+    public function godog() {
+      echo "jadinya mboh opo ngelu aku astaghfirullah";
+    }
+}
